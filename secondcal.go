9347645adcbb66d27cf3776fc886f9af407cb382
Anshul Tiwari <anshtiwari0710@gmail.com>
2020-07-17 16:49:25 +0530

    secondcal.go file added

---
+++ secondcal.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func calc(x, y int) (int, int) {
+	var out1 = x + y
+	var out2 = x - y
+	return out1, out2
+}
+func main() {
+	num1 := 5
+	num2 := 4
+	result1, result2 := calc(num1, num2)
+	fmt.Println(result1, result2)
+}
